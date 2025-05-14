7633234db53249b8fe9b1b2bcc6a07367f86780d
Matinz03 <matinpsn1@gmail.com>
2025-05-14 18:02:01 +0200

    fix(token): add safe supabase token

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabaseUrl = 'https://boexjtaembwyqvzipxbr.supabase.co'
-<<<<<<< HEAD
+
 const supabaseKey =
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvZXhqdGFlbWJ3eXF2emlweGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MDQ1ODcsImV4cCI6MjA1ODk4MDU4N30.kb7V_yCkByOIM7BFboboUQv8iaGC-4QF89PpvXwKbgc'
-=======
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvZXhqdGFlbWJ3eXF2emlweGJyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM0MDQ1ODcsImV4cCI6MjA1ODk4MDU4N30.kb7V_yCkByOIM7BFboboUQv8iaGC-4QF89PpvXwKbgc'
->>>>>>> 6309c219066fbc7aa85b2acc36545e59a0816504
+
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase
